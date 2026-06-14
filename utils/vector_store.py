273a17ab60437d0a3db2491558e9e2bf986ee705
iamcoolkorean <iamcoolkorean@gmail.com>
2026-06-14 15:59:47 +0900

    Update vector_store.py

--- utils/vector_store.py
+++ utils/vector_store.py
@@ -7,15 +7,17 @@ class RotatingEmbeddings(GoogleGenerativeAIEmbeddings):
     """임베딩 요청마다 다른 키를 사용"""
     def embed_documents(self, texts, **kwargs):
         temp_emb = GoogleGenerativeAIEmbeddings(
-            model="models/embedding-001",
+            model="models/text-embedding-004",
             google_api_key=key_manager.get_next_key()
+)
         )
         return temp_emb.embed_documents(texts, **kwargs)
 
     def embed_query(self, text, **kwargs):
         temp_emb = GoogleGenerativeAIEmbeddings(
-            model="models/embedding-001",
+            model="models/text-embedding-004",
             google_api_key=key_manager.get_next_key()
+)
         )
         return temp_emb.embed_query(text, **kwargs)
 
